e9a1ce5212415cefcb1177029e1d3a91e0b847e3
gabriellm1 <gabriellopesmonteiro41@gmail.com>
2019-11-18 18:55:45 -0300

    fix

--- flask_redir.py
+++ flask_redir.py
@@ -1,25 +1,26 @@
 from flask import Flask
 from flask import request
+from flask.json import jsonify
 import requests
 
 app = Flask(__name__)
 
-ip = #endir
+ip = #end
 
 @app.route('/posts',methods=['POST'])
 def createMarkers():
 	content = request.get_json()
 	r = requests.post(url="http://"+ip+":4444/posts",json=content)
-	return r.json()
+	return r.json()[0]
 
 @app.route('/',methods=['GET'])
 def markers():
 	r = requests.get(url="http://"+ip+":4444/")
-	return r.json()[0]
+	return jsonify(r.json()[0])
 
 
 
 
 if __name__ == "__main__":
 
-	app.run()
+	app.run(host="0.0.0.0")
