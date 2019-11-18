58ec349ec742d9aed5d49246e22cb0dfc22db1c0
gabriellm1 <gabriellopesmonteiro41@gmail.com>
2019-11-17 23:48:17 -0300

    init

---
+++ flask_redir.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask import request
+import requests
+
+app = Flask(__name__)
+
+ip = #endir
+
+@app.route('/posts',methods=['POST'])
+def createMarkers():
+	content = request.get_json()
+	r = requests.post(url="http://"+ip+":4444/posts",json=content)
+	return r.json()
+
+@app.route('/',methods=['GET'])
+def markers():
+	r = requests.get(url="http://"+ip+":4444/")
+	return r.json()[0]
+
+
+
+
+if __name__ == "__main__":
+
+	app.run()
