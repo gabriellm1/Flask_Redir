6a16214d86dc6391b4bcd56209c94fc6b790f738
gabriellm1 <gabriellopesmonteiro41@gmail.com>
2019-11-19 15:10:29 -0300

    done

--- flask_redir.py
+++ flask_redir.py
@@ -1,3 +1,4 @@
+
 from flask import Flask
 from flask import request
 from flask.json import jsonify
@@ -9,18 +10,23 @@ ip = #end
 
 @app.route('/posts',methods=['POST'])
 def createMarkers():
-	content = request.get_json()
-	r = requests.post(url="http://"+ip+":4444/posts",json=content)
-	return r.json()[0]
+        content = request.get_json()
+        r = requests.post(url="http://"+ip+":4444/posts",json=content)
+        return jsonify(r.json())
 
 @app.route('/',methods=['GET'])
 def markers():
-	r = requests.get(url="http://"+ip+":4444/")
-	return jsonify(r.json()[0])
+        r = requests.get(url="http://"+ip+":4444/")
+        return jsonify(r.json()[0])
 
+
+@app.route('/healthcheck',methods=['GET'])
+def healthcheck():
+        return jsonify({"state":"good"})
 
 
 
 if __name__ == "__main__":
 
-	app.run(host="0.0.0.0")
+        app.run(host="0.0.0.0")
+
