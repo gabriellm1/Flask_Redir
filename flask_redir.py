42a9e71e98b590a652c7c13e65e6cb61adbee291
gabriellm1 <gabriellopesmonteiro41@gmail.com>
2019-11-19 15:47:28 -0300

    teste

--- flask_redir.py
+++ flask_redir.py
@@ -11,12 +11,12 @@ ip = #end
 @app.route('/posts',methods=['POST'])
 def createMarkers():
         content = request.get_json()
-        r = requests.post(url="http://"+ip+":4444/posts",json=content)
+        r = requests.post(url="http://"+str(ip)+"/posts",json=content)
         return jsonify(r.json())
 
 @app.route('/',methods=['GET'])
 def markers():
-        r = requests.get(url="http://"+ip+":4444/")
+        r = requests.get(url="http://"+str(ip)+"/")
         return jsonify(r.json()[0])
 
 
